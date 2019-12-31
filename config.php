e8baa9693db40191f7970f53464a87a7f3895d41
arata <contact@materializing.net>
2019-12-31 21:14:04 +0900

    clean 整形

--- config.php
+++ config.php
@@ -2,14 +2,13 @@
 /**
  * [ADMIN] MailSignSwitch
  *
- * @link			http://www.materializing.net/
- * @author			arata
- * @license			MIT
+ * @link http://www.materializing.net/
+ * @author arata
+ * @license MIT
  */
 $title = 'メールサインスウィッチ';
 $description = 'メールフォーム別に署名内容を設定できます。';
 $author = 'arata';
 $url = 'http://www.materializing.net/';
 //$adminLink = array('plugin' => 'mail', 'controller' => 'mail_contents', 'action' => 'index');
-$installMessage = 'メールフォーム設定編集画面に、署名設定欄が追加されます。';
-$installMessage .= '<br />初期値として、メールプラグイン基本設定 で設定中の内容が反映されます。';
+$installMessage = 'メールフォーム設定編集画面に、署名設定欄が追加されます。初期値として、メールプラグイン基本設定 で設定中の内容が反映されます。';
