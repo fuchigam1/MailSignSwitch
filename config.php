44d9ce4239efa472ee675190d502bb98a7e3e432
arata <contact@materializing.net>
2020-01-01 19:20:51 +0900

    fix 説明内容修正

--- config.php
+++ config.php
@@ -11,4 +11,4 @@ $description = 'メールフォーム別に署名内容を設定できます。'
 $author = 'arata';
 $url = 'http://www.materializing.net/';
 //$adminLink = array('plugin' => 'mail', 'controller' => 'mail_contents', 'action' => 'index');
-$installMessage = 'メールフォーム設定編集画面に、署名設定欄が追加されます。初期値として、メールプラグイン基本設定 で設定中の内容が反映されます。';
+$installMessage = 'メールフォーム設定編集画面に、署名設定欄が追加されます。';
